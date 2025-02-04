a3c703e6bf13dcd85709fd200e20f9668c02db80
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2025-02-04 18:10:19 +0600

    fix new conn notify issue

--- src/modules/server/connection.rs
+++ src/modules/server/connection.rs
@@ -1,7 +1,7 @@
 use crate::{
     helpers::lock::{
         addr::remove_socket_addr,
-        users::{get_all_other_users, get_all_others, get_all_users_and_senders},
+        users::{get_all_other_users, get_all_others, get_all_users_and_senders, get_user},
     },
     models::user::{UserInfo, WSUsers},
 };
@@ -15,10 +15,6 @@ pub fn close_connection_notify(addr: &SocketAddr) -> Result<()> {
     match get_all_users_and_senders() {
         Some(users_senders) => {
             for (u, sender) in users_senders {
-                // let msg = u.unwrap().addr(Some(*addr)).to_ws_user();
-                // UserInfo::new(Some(u.unwrap().user_id), None, Some(*addr)).to_dis_conn()
-                // .to_json()
-                // .to_ws_msg_text();
                 let msg = WSUsers::new(Some(false), Some(vec![u.unwrap().clone()])).to_ws();
                 sender.unwrap().send(msg)?;
             }
@@ -30,24 +26,25 @@ pub fn close_connection_notify(addr: &SocketAddr) -> Result<()> {
 
 pub fn new_connection_notify(addr: &SocketAddr) -> Result<()> {
     let others = get_all_others(&addr);
-    // println!("others: {:?}", others);
+    println!("others: {:?}", others);
     match others.is_empty() {
         true => println!("No recipient found"),
-        false => match others.last() {
-            Some((user, recp)) => match user {
-                Some(u) => {
-                    println!("New connection from: {:?}", user);
-                    let msg = WSUsers::new(Some(true), Some(vec![u.clone()])).to_ws();
-                    // let msg = u.to_ws_user();
-                    match recp {
-                        Some(r) => r.send(msg)?,
-                        None => println!("No recipient found"),
+        false => {
+            let new_user = get_user(addr);
+            for (_, s) in others {
+                match new_user.clone() {
+                    Some(u) => {
+                        let msg = WSUsers::new(Some(true), Some(vec![u.clone()])).to_ws();
+
+                        match s {
+                            Some(r) => r.send(msg)?,
+                            None => println!("No recipient found"),
+                        }
                     }
+                    None => println!("No recipient found"),
                 }
-                None => println!("No recipient found"),
-            },
-            None => println!("No recipient found"),
-        },
+            }
+        }
     }
 
     Ok(())
